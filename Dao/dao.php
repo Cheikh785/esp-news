4d7585fa265cd333dfd736ab694cd639a0d00237
Cheikh785 <cheikhomar.cof@gmail.com>
2022-08-04 00:20:40 +0000

    configuration of the dao for the hosting

--- Dao/dao.php
+++ Dao/dao.php
@@ -4,7 +4,7 @@
     $password = "passer";
     function connect_db() {
         try {
-            $conn = new PDO("mysql:host=localhost;dbname=esp_news", 'root', 'passer');
+            $conn = new PDO("mysql:host=ec2-52-48-159-67.eu-west-1.compute.amazonaws.com;dbname=deb68r2nev2dc8", 'nxhsgshhbkkcrq', '6214a80eae510711ae48371277f3bab97e3fdb9a1c028acf3cdb8438c895342e');
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         } catch(PDOException $e) {
