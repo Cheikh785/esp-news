8c6c5f7e3121ed325e213376fbf05ebbfd6db2c7
Cheikh785 <cheikhomar.cof@gmail.com>
2022-08-05 22:51:16 +0000

    creating & styling administrator page

--- administrateur.php
+++ administrateur.php
@@ -7,19 +7,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page d'édition</title>
+    <title>Page d'administrateur</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet"  href="css/bootstrap.css"/>
-    <link rel="stylesheet" href="css/editeur.css">
+    <link rel="stylesheet" href="css/administrateur.css">
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-8 title">
-                <h1>Bienvenu Mbam Xoux</h1>
+            <div class="col-10">
+                <div class="row">
+                    <div class="col-9 title">
+                        <h1>Bienvenu Mbam Xoux</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-2 col-md-3 col-lg-2 col-xl-2 categorie">
+                        <ul>
+                            <li class="active"><a href="#">Accueil</a></li>
+                            <li class=""><a href="#">Sport</a></li>
+                            <li class=""><a href="#">Santé</a></li>
+                            <li class=""><a href="#">Education</a></li>
+                            <li class=""><a href="#">Politique</a></li>
+                        </ul>
+                    </div>
+                    <?php
+                    if (!$estVide) { ?>
+                        <div class="col-sm-9 col-md-8 col-lg-9 col-xl-9 container-article">
+                        <?php
+                            foreach($listeAllArticles as $article) { ?>
+                                <div class="article">
+                                    <p class="titre"><?php echo $article['titre']; ?></p>
+                                    <p class="contenu"><?= substr($article['contenu'], 0, 900);  ?></p>
+                                    <div class="icone">
+                                        <a href="#" class="edit" data-bs-toggle="modal" data-bs-target="#editArticleModal"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg></a>
+                                        <a href="#" class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></a>
+                                    </div>
+                                </div> <?php } ?>
+                            <!-- <div class="container">
+                                <button class="precedent"> &lt;&lt; Précédent</button>
+                                <button class="suivant">Suivant &gt;&gt;</button>
+                            </div> -->
+                        </div> <?php 
+                    } else {    ?> 
+                        <div class=" container alert"><p> Aucun article disponible !</p></div>  <?php 
+                    }   ?>
+                </div>
             </div>
-            <div class="col">
+            <div class="col-2 adding">
                 <div class="row">
                     <div class="col ajouter-categorie">
                         <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCategoryModal">Ajouter une catégorie</button>
@@ -27,50 +63,20 @@
                 </div>
                 <div class="row">
                     <div class="col modifier-categorie">
-                        <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editCategoryModal">Modifier une categorie</button>
+                        <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editCategoryModal">Modifier une categorie</button>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col ajouter-article">
-                        <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#addArticleModal">Ajouter un article</button>
+                        <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#addArticleModal">Ajouter un article</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col ajouter-article">
+                        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#">Liste des Utilisateurs</button>
                     </div>
                 </div>
             </div>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-2 col-md-3 col-lg-2 col-xl-2 categorie">
-                <ul>
-                    <li class="active"><a href="#">Accueil</a></li>
-                    <li class=""><a href="#">Sport</a></li>
-                    <li class=""><a href="#">Santé</a></li>
-                    <li class=""><a href="#">Education</a></li>
-                    <li class=""><a href="#">Politique</a></li>
-                </ul>
-            </div>
-            <?php
-                if (!$estVide) { ?>
-                    <div class="col-sm-9 col-md-8 col-lg-9 col-xl-9 container-article">
-                    <?php
-                        foreach($listeAllArticles as $article) { ?>
-                            <div class="article">
-                                <p class="titre"><?php echo $article['titre']; ?></p>
-                                <p class="contenu"><?= substr($article['contenu'], 0, 900);  ?></p>
-                                <div class="icone">
-                                    <a href="#" class="edit" data-bs-toggle="modal" data-bs-target="#editArticleModal"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg></a>
-                                    <a href="#" class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg></a>
-                                </div>
-                            </div> <?php } ?>
-                        <!-- <div class="container">
-                            <button class="precedent"> &lt;&lt; Précédent</button>
-                            <button class="suivant">Suivant &gt;&gt;</button>
-                        </div> -->
-                    </div>
-            <?php 
-            } else {
-                ?> <div class=" container alert"><p> Aucun article disponible !</p></div>
-                <?php }
-            ?>
         </div>
     </div>
     
