362e8fa699b06238b5b175a81b9cd708947423ad
Cheikh785 <cheikhomar.cof@gmail.com>
2022-08-10 20:38:33 +0000

    changing accueil image for the readme file

--- Dao/dao.php
+++ Dao/dao.php
@@ -1,10 +1,26 @@
 <?php
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "passer";
+    // function connect_db() {
+    //     try {
+    //         $conn = new PDO("pgsql:host=ec2-52-48-159-67.eu-west-1.compute.amazonaws.com;dbname=deb68r2nev2dc8;port=5432", 'nxhsgshhbkkcrq', '6214a80eae510711ae48371277f3bab97e3fdb9a1c028acf3cdb8438c895342e');
+    //         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //         return $conn;
+    //     } catch(PDOException $e) {
+    //         echo "Connection failed: " . $e->getMessage();
+    //     }
+    // }
+?>
+
+
+<?php
     $servername = "localhost";
     $username = "root";
     $password = "passer";
     function connect_db() {
         try {
-            $conn = new PDO("pgsql:host=ec2-52-48-159-67.eu-west-1.compute.amazonaws.com;dbname=deb68r2nev2dc8;port=5432", 'nxhsgshhbkkcrq', '6214a80eae510711ae48371277f3bab97e3fdb9a1c028acf3cdb8438c895342e');
+            $conn = new PDO("mysql:host=localhost;dbname=esp_news", 'root', 'passer');
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         } catch(PDOException $e) {
