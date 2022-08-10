4bddcaf990d9b39934803c87a38e463b1b8d9d45
Cheikh785 <cheikhomar.cof@gmail.com>
2022-08-10 20:54:39 +0000

    changing accueil image for the readme file*

--- navbar.php
+++ navbar.php
@@ -3,10 +3,10 @@
 ?>
 <nav class="navbar">
     <ul id="liste">
-        <li class="accueil active"><a href="/index.php?action=accueil"><?php echo $tabCategories[0]['libelle']; ?></a></li>
-        <li class="sport"><a href="/index.php?action=sport"><?php echo $tabCategories[1]['libelle']; ?></a></li>
-        <li class="sante"><a href="/index.php?action=education"><?php echo $tabCategories[2]['libelle']; ?></a></li>
-        <li class="education"><a href="/index.php?action=sante" onclick="handdleEducation()"><?php echo $tabCategories[3]['libelle']; ?></a></li>
-        <li class="politique"><a href="/index.php?action=politique" onclick="handdlePolitique()"><?php echo $tabCategories[4]['libelle']; ?></a></li>
+        <li class="accueil active"><a href="index.php?action=accueil"><?php echo $tabCategories[0]['libelle']; ?></a></li>
+        <li class="sport"><a href="index.php?action=sport"><?php echo $tabCategories[1]['libelle']; ?></a></li>
+        <li class="sante"><a href="index.php?action=education"><?php echo $tabCategories[2]['libelle']; ?></a></li>
+        <li class="education"><a href="index.php?action=sante" onclick="handdleEducation()"><?php echo $tabCategories[3]['libelle']; ?></a></li>
+        <li class="politique"><a href="index.php?action=politique" onclick="handdlePolitique()"><?php echo $tabCategories[4]['libelle']; ?></a></li>
     </ul>
 </nav>
